2b5301e8074516566e297c3714fd05b485e8e3f8
Rob Lehmann <lehmannrobb@gmail.com>
2023-03-14 14:35:30 +0300

    Update build script and removed colors package

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import Colors = require("colors.ts");
 
 const MONG_URI: string = process.env.MONGO_URI ?? "";
 
@@ -8,12 +7,7 @@ const connectDB = async () => {
     mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(MONG_URI);
 
-    console.log(
-      Colors.colors(
-        ["cyan", "underline"],
-        `MongoDB Connected: ${conn.connection.host}`
-      )
-    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
